79ed606b47d38c06699ad32e5683eacaf198b652
Sudhakar <patamsudhakarit@gmail.com>
2018-10-30 12:39:08 +0530

    Added code for TouchId

---
+++ TestiOSApp/ViewController.swift
@@ -0,0 +1,116 @@
+//
+//  ViewController.swift
+//  TestiOSApp
+//
+//  Created by apple on 21/10/18.
+//  Copyright © 2018 apple. All rights reserved.
+//
+
+import UIKit
+import LocalAuthentication
+import SystemConfiguration
+
+struct KeychainPasswordItem {
+
+}
+
+enum LAError : Int {
+    case AuthenticationFailed
+    case UserCancel
+    case UserFallback
+    case SystemCancel
+    case PasscodeNotSet
+    case TouchIDNotAvailable
+    case TouchIDNotEnrolled
+}
+
+class ViewController: UIViewController {
+
+    var str : String?
+    let context = LAContext()
+
+    @IBOutlet weak var tfd_Username: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let ipAddress = getIFAddresses()
+        print(ipAddress)
+
+
+    }
+
+    @IBAction func touchId(_ sender: Any) {
+
+
+        self.authenticateUser()
+
+
+    }
+
+    func getIFAddresses() -> [String] {
+        var addresses = [String]()
+
+        // Get list of all interfaces on the local machine:
+        var ifaddr : UnsafeMutablePointer<ifaddrs>?
+        guard getifaddrs(&ifaddr) == 0 else { return [] }
+        guard let firstAddr = ifaddr else { return [] }
+
+        // For each interface ...
+        for ptr in sequence(first: firstAddr, next: { $0.pointee.ifa_next }) {
+            let flags = Int32(ptr.pointee.ifa_flags)
+            let addr = ptr.pointee.ifa_addr.pointee
+
+            // Check for running IPv4, IPv6 interfaces. Skip the loopback interface.
+            if (flags & (IFF_UP|IFF_RUNNING|IFF_LOOPBACK)) == (IFF_UP|IFF_RUNNING) {
+                if addr.sa_family == UInt8(AF_INET) || addr.sa_family == UInt8(AF_INET6) {
+
+                    // Convert interface address to a human readable string:
+                    var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                    if (getnameinfo(ptr.pointee.ifa_addr, socklen_t(addr.sa_len), &hostname, socklen_t(hostname.count),
+                                    nil, socklen_t(0), NI_NUMERICHOST) == 0) {
+                        let address = String(cString: hostname)
+                        addresses.append(address)
+                    }
+                }
+            }
+        }
+
+        freeifaddrs(ifaddr)
+        return addresses
+    }
+
+    func authenticateUser() {
+
+        let context = LAContext()
+        var error: NSError?
+
+        // 2
+        // check if Touch ID is available
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // 3
+            let reason = "Authenticate with Touch ID"
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason, reply:
+                {(succes, error) in
+                    // 4
+                    if succes {
+                        self.showAlertController("Touch ID Authentication Succeeded")
+                    }
+                    else {
+                        self.showAlertController("Touch ID Authentication Failed")
+                    }
+                    } as (Bool, Error?) -> Void)
+        }
+            // 5
+        else {
+            showAlertController("Touch ID not available")
+        }
+    }
+    func showAlertController(_ message: String) {
+        let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+}
+
+
---
+++ TestiOSAppTests/TestiOSAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  TestiOSAppTests.swift
+//  TestiOSAppTests
+//
+//  Created by apple on 21/10/18.
+//  Copyright © 2018 apple. All rights reserved.
+//
+
+import XCTest
+@testable import TestiOSApp
+
+class TestiOSAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
